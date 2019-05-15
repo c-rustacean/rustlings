b9885242b816ab6f03a85f2d1a936acee59226bd
Eddy Petrisor <eddy.petrisor@nxp.com>
2019-05-15 20:47:03 +0300

    resolved until exercises/error_handling/errorsn.rs
    Signed-off-by: Eddy Petrisor <eddy.petrisor@nxp.com>

--- exercises/error_handling/errors3.rs
+++ exercises/error_handling/errors3.rs
@@ -10,14 +10,17 @@ fn main() {
     let mut tokens = 100;
     let pretend_user_input = "8";
 
-    let cost = total_cost(pretend_user_input)?;
-
-    if cost > tokens {
-        println!("You can't afford that many!");
+    if let Ok(cost) = total_cost(pretend_user_input) {
+        if cost > tokens {
+            println!("You can't afford that many!");
+        } else {
+            tokens -= cost;
+            println!("You now have {} tokens.", tokens);
+        }
     } else {
-        tokens -= cost;
-        println!("You now have {} tokens.", tokens);
+        println!("Error parsing input {}", pretend_user_input);
     }
+
 }
 
 pub fn total_cost(item_quantity: &str) -> Result<i32, ParseIntError> {
