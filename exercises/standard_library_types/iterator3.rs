92e288670a5c41a538241faba2ecad72d59cf28d
Eddy Petrisor <eddy.petrisor@nxp.com>
2019-05-21 19:39:59 +0300

    iterators3.rs: Implemented the even divide and fixed the commented tests
    Signed-off-by: Eddy Petrisor <eddy.petrisor@nxp.com>

--- exercises/standard_library_types/iterator3.rs
+++ exercises/standard_library_types/iterator3.rs
@@ -24,6 +24,11 @@ pub struct NotDivisibleError {
 // evenly divisible by b.
 // Otherwise, it should return a suitable error.
 pub fn divide(a: i32, b: i32) -> Result<i32, DivisionError> {
+    if b == 0 { return Err(DivisionError::DivideByZero)};
+    match a % b {
+        0 => Ok(a/b),
+        _ => Err(DivisionError::NotDivisible(NotDivisibleError{dividend: a, divisor: b})),
+    }
 }
 
 #[cfg(test)]
@@ -58,12 +63,11 @@ mod tests {
     }
 
     // Iterator exercises using your `divide` function
-    /*
     #[test]
     fn result_with_list() {
         let numbers = vec![27, 297, 38502, 81];
         let division_results = numbers.into_iter().map(|n| divide(n, 27));
-        let x //... Fill in here!
+        let x: Result<Vec<_>, _> = division_results.collect();
         assert_eq!(format!("{:?}", x), "Ok([1, 11, 1426, 3])");
     }
 
@@ -71,10 +75,9 @@ mod tests {
     fn list_of_results() {
         let numbers = vec![27, 297, 38502, 81];
         let division_results = numbers.into_iter().map(|n| divide(n, 27));
-        let x //... Fill in here!
+        let x = division_results.collect::<Vec<Result<_, _>>>();
         assert_eq!(format!("{:?}", x), "[Ok(1), Ok(11), Ok(1426), Ok(3)]");
     }
-    */
 }
 
 
